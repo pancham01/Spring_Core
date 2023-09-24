380d54a9814233aa46f97e82d36dc0a280ae20c7
pancham01 <pancham9557@gmail.com>
2023-09-24 17:20:40 +0530

    update

--- src/main/java/spring_core/dpInjectPro/Entity/Employee.java
+++ src/main/java/spring_core/dpInjectPro/Entity/Employee.java
@@ -1,42 +1,17 @@
 package spring_core.dpInjectPro.Entity;
 
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+@Data
+@AllArgsConstructor
 public class Employee {
 	private String name;
-	private int id;
-
-	public Employee() {
-		super();
-	}
-
-	public Employee(String name, int id) {
-		super();
-		this.name = name;
-		this.id = id;
-		System.out.println("Param-called");
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-		System.out.println("Name-setter");
-	}
-
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-		System.out.println("Id-setter");
-
-	}
-
-	@Override
-	public String toString() {
-		return "Employee [name=" + name + ", id=" + id + "]";
-	}
+	private List<String> address;
+	private Set<Integer> phone;
+	private Map<Integer,String> map;
 
 }
