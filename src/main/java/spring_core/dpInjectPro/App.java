5d2512ce00cbd4bb40fc7a00418f73d68bb6e97b
pancham01 <pancham9557@gmail.com>
2023-09-28 00:20:15 +0530

    update

--- src/main/java/spring_core/dpInjectPro/App.java
+++ src/main/java/spring_core/dpInjectPro/App.java
@@ -3,6 +3,7 @@ package spring_core.dpInjectPro;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import spring_core.dpInjectPro.Entity.Child;
 import spring_core.dpInjectPro.Entity.Employee;
 
 /**
@@ -16,5 +17,12 @@ public class App
     	ApplicationContext factory = new ClassPathXmlApplicationContext("ApplicationContext.xml");
     	Employee e =(Employee)factory.getBean("emp2");
     	System.out.println(e);
+    	
+    	
+    	//Parent Child
+    	ApplicationContext bean = new ClassPathXmlApplicationContext("parent_child.xml");
+    	Child e2 =(Child)bean.getBean("child");
+    	System.out.println(e2);
+    	
     }
 }
--- src/main/java/spring_core/dpInjectPro/Entity/Employee.java
+++ src/main/java/spring_core/dpInjectPro/Entity/Employee.java
@@ -1,9 +1,5 @@
 package spring_core.dpInjectPro.Entity;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
