92b32e55ad22e325e539cd910c0bc1ffbc161eaf
pancham01 <pancham9557@gmail.com>
2025-03-23 18:40:36 +0530

    update

--- src/main/java/spring_core/dpInjectPro/client/_0_App.java
+++ src/main/java/spring_core/dpInjectPro/client/_0_App.java
@@ -22,16 +22,16 @@ public class _0_App {
 //
 //		System.out.println("----------");
 //
-		Address addBean1 = app.getBean(Address.class);
-		Address addBean2 = app.getBean(Address.class);
-		System.out.println(addBean1);
-		System.out.println(addBean2);
-		
-
+//		Address addBean1 = app.getBean(Address.class);
+//		Address addBean2 = app.getBean(Address.class);
+//		System.out.println(addBean1);
+//		System.out.println(addBean2);
+//		
 //
-//		Employee bean1 = app.getBean(Employee.class);
-//		System.out.println(bean1);
-		 ((AbstractApplicationContext)app).registerShutdownHook();
+////
+////		Employee bean1 = app.getBean(Employee.class);
+////		System.out.println(bean1);
+//		 ((AbstractApplicationContext)app).registerShutdownHook();
 //		
 	}
 }
