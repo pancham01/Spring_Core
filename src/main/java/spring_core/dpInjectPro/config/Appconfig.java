1319f55262962f2f992c17054038b2450b97c386
pancham01 <pancham9557@gmail.com>
2025-05-24 13:02:12 +0530

    update

--- src/main/java/spring_core/dpInjectPro/config/Appconfig.java
+++ src/main/java/spring_core/dpInjectPro/config/Appconfig.java
@@ -1,25 +1,14 @@
 package spring_core.dpInjectPro.config;
 import javax.sql.DataSource;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 @Configuration
 @ComponentScan("spring_core.dpInjectPro")
-//@PropertySource("classpath:database.properties")
 public class Appconfig {
-
-	@Autowired
-	Environment environment;
-
-	private final String URL = "url";
-	private final String USER = "dbuser";
-	private final String DRIVER = "driver";
-	private final String PASSWORD = "dbpassword";
 
 	@Bean
 	DataSource dataSource() {
