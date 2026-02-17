a377fb0b868954b71520d7274f32d27849cb1398
pancham01 <108358732+pancham01@users.noreply.github.com>
2026-02-17 14:48:05 +0530

    update

--- src/main/java/spring_core/dpInjectPro/client/App.java
+++ src/main/java/spring_core/dpInjectPro/client/App.java
@@ -14,23 +14,12 @@ public class App {
 	public static void main(String[] args) {
 		ApplicationContext ioc = new ClassPathXmlApplicationContext("application.xml");
 		EmployeeDao edao = ioc.getBean("edao", EmployeeDao.class);
-		
-		
-		Employee emp1 = new Employee(3, "Nakul","Male", 96000);
+		Employee emp1 = new Employee(2, "Mukul Sharma","Male", 888000);
 //		edao.saveEmployee(emp1);
-		
-		
-//    	edao.updateEmployee(emp1);
-//    	System.out.println("updated successfully");
-//    	edao.deleteEmployee(7);
-//    	edao.deleteEmployee(9);
-
-		List<Employee> allEmp=edao.getAllEmp();
-//		
-		for (Employee employee : allEmp) {
-			System.out.println(employee);
-		}
-		System.out.println("Get successfully");
+//		edao.updateEmployee(emp1);
+//		edao.deleteEmployee(2);
+		List<Employee> allEmp = edao.getAllEmp();
+		System.out.println(allEmp);
 
 	}
 }
